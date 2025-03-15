008aa94b4aec009f8fb56f99f15adcaf07c46214
winston-majano <winstonmajano@gmail.com>
2025-03-15 18:47:47 +0100

    Subiendo componente de listado de productos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ListadoProductosComponent } from "./listado-productos/listado-productos.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ListadoProductosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/listado-productos/listado-productos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-productos',
+  imports: [],
+  templateUrl: './listado-productos.component.html',
+  styleUrl: './listado-productos.component.css'
+})
+export class ListadoProductosComponent {
+
+}
